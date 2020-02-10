34ee70b0c8da5499db7878ac09306355ae46cae8
4ngu3te <60628775+4ngu3te@users.noreply.github.com>
2020-02-10 19:45:47 -0300

    IF EN CLASE

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,17 @@
-function mostrar()
-{
 //tomo la edad  
 
+function mostrar()
+{
+	var edad
+
+	edad=document.getElementById('edad').value;
+
+	if(edad==15)
+	{
+		alert("Niña bonita");
+	}
+
+}
 
 
-}//FIN DE LA FUNCIÓN
+
