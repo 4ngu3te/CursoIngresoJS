d95239011a07cf9458d0ca568700a0c95be862ec
4ngu3te <f.garancini15@gmail.com>
2020-02-11 20:18:10 -0300

    Random

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,7 +1,9 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	alert(Math.floor((Math.random() * 10) + 1));
-
+	//alert(Math.floor((Math.random() * 10) + 1));
+	var random
+	random=Math.floor((Math.random() * 10) + 1)
+	console.log(random)
 
 }//FIN DE LA FUNCIÓN
