414b27f75d617e32311a7ff256de4cb1aa0cc1b7
4ngu3te <60628775+4ngu3te@users.noreply.github.com>
2020-02-10 22:00:57 -0300

    HECHO EN CLASE IF ELSE

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+var edad
 
-
-
-
+edad=document.getElementById('edad').value;
+if(edad > 17)
+	{
+		alert("Es Adolecente");
+	}else{
+		if(edad < 13)
+		{
+			alert("Es un niño");
+		}else
+			{
+				alert("Es un adulto");
+			}
+		}
 }//FIN DE LA FUNCIÓN
