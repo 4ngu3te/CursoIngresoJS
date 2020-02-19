ca9371422cdd946a63d8a04212bef43600acb976
4ngu3te <f.garancini15@gmail.com>
2020-02-18 21:02:30 -0300

    while en clase

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
-
+var contador;
 var clave = prompt("ingrese el número clave.");
+
+	while(clave!="utn750")
+	{
+		contador=3
+		clave=prompt("Error")
+	}
+
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,6 +2,13 @@ function mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
+	numero=parseInt(numero);
 
+	while(numero<0 || numero>10)
+	{
+		var numero = prompt("Error, reingrese un número entre 0 y 10.");
+		
+	}
+	document.getElementById('Numero').value=numero
 
 }//FIN DE LA FUNCIÓN
