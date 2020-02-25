21fba8f4aff6ffee3b168347ab0976e357e1fba4
4ngu3te <60628775+4ngu3te@users.noreply.github.com>
2020-02-25 19:58:07 -0300

    parcialito en casa

---
+++ 9-Parciales/ocho.js
@@ -0,0 +1,91 @@
+function mostrar()
+{
+    var letra;
+    var numero;
+    var pares;
+    var respuesta='si';
+    var impares;
+    var ceros;
+    var positivos;
+    var contador;
+    var promedios;
+    var negativos;
+    var maximo;
+    var letramax;
+    var num_max;
+    maximo=0;
+    negativos=0;
+    contador=0;
+    positivos=0;
+    ceros=0;
+    impares=0;
+    pares=0;
+    
+    while (respuesta=='si')
+    {
+        letra=prompt('Ingrese una letra');
+        numero=prompt('Ingrese un numero');
+        numero=parseInt(numero);
+            while (numero<-100 || numero>100) 
+            {
+                numero=prompt('Ingrese un numero valido'); 
+            }
+            if(numero%2==0)
+            {
+                pares++;
+            }else
+            {
+                impares++;
+            }
+            if (numero>0)
+            {
+                positivos=positivos+numero;
+            }else
+            {
+                negativos=negativos+numero;
+            }  
+            if (numero>maximo) 
+            {
+                maximo=numero;
+                letramax=letra;
+            }
+
+            switch (numero) {
+                case 10:
+                case 20:
+                case 30:
+                case 40:
+                case 50:
+                case 60:
+                case 70:
+                case 80:
+                case 90:
+                case -10:
+                case -20:
+                case -30:
+                case -40:
+                case -50:
+                case -60:
+                case -70:
+                case -80:
+                case -90:
+                    ceros++;  
+                    break;
+                case 100:
+                case -100:
+                    ceros=+2;
+                default:
+                    break;
+           
+            }
+        respuesta=prompt('desea seguir ingresando datos?');
+        contador++;
+        promedios=positivos/contador;
+            console.log("Pares: "+pares);
+            console.log("Impares: "+impares);
+            console.log("Ceros: "+ceros);
+            console.log("Promedios: "+promedios);
+            console.log("Suma de Negativos: "+negativos);
+            console.log("Letra y numero maximo:"+letramax+''+maximo);
+    }
+}
